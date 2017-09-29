af311d933c2b4f946a1234583e93a5392cc6362b
香饽饽 <1377699408@qq.com>
2017-09-29 15:42:51 +0800

    增加拦截器配置

--- src/main/java/com/cheungchan/highlight_springmvc4/MyMvcConfig.java
+++ src/main/java/com/cheungchan/highlight_springmvc4/MyMvcConfig.java
@@ -1,9 +1,11 @@
 package com.cheungchan.highlight_springmvc4;
 
+import com.cheungchan.highlight_springmvc4.interceptor.DemoInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -26,5 +28,14 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/assets/**")
                 .addResourceLocations("classpath:/assets/");
+    }
+    @Bean
+    public DemoInterceptor demoInterceptor(){
+        return new DemoInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(demoInterceptor());
     }
 }
